3e0888491d0176fcfab1187fd973c8dffc208b92
colloso999 <tripperskripper@gmail.com>
2023-01-04 10:48:21 +0300

    call a method on each elemnt

--- dataStructures/lists/2_listDataType.py
+++ dataStructures/lists/2_listDataType.py
@@ -12,3 +12,7 @@ print([x for x in vec if x>=0])
 
 # apply a function to all the elements
 print([abs(x) for x in vec])
+
+# call a method on each element
+freshfruit = ['banana  ', ' loganberry ', 'passion fruit ']
+print([weapon.strip() for weapon in freshfruit])
