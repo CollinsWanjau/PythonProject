f58493c762c46ed25e9377a95226455146cc284b
colloso999 <tripperskripper@gmail.com>
2023-01-05 12:36:46 +0300

    used built-in function zip

--- dataStructures/lists/nestedListComp.py
+++ dataStructures/lists/nestedListComp.py
@@ -11,3 +11,23 @@ matrix = [
 
 # the following list comprehension will transpose rows and columns
 print([[row[i] for row in matrix] for i in range(4)])
+
+# as we saw before, the innermost comprehension is evaluated in the context of the for that
+# follows it
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+print(transposed)
+
+# which in turn is the same as
+transposed = []
+for i in range(4):
+    # the following lines implement the nested listcomp
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+print(transposed)
+
+# in the real world you should use the built-in function zip()
+print(list(zip(*matrix)))
