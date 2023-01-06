8529c8683c8cecc71e2eb70e7bc6cc1e570fd530
colloso999 <tripperskripper@gmail.com>
2023-01-06 12:48:31 +0300

    list comprehensions

---
+++ dataStructures/lists/6_listComprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import random
+import math
+
+# perform an operation on each item in the list
+# create a list of even values
+evenList = [i*2 for i in range(10)]
+
+for i in evenList:
+    print(i)
