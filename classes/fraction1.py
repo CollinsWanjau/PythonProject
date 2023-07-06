84160b7fb7661deae43e1c8441df5e7903551ecb
colloso999 <tripperskripper@gmail.com>
2023-07-06 09:47:56 +0300

    defining a static gcd function

---
+++ classes/fraction1.py
@@ -0,0 +1,22 @@
+class fraction(object):
+    """
+    The static method "gcd" is called by our class method "reduce" with "cls.gcd(n1, n2)"
+
+    "cls" is a reference to a fraction
+    """
+
+
+    def __init__(self, n, d):
+        self.numerator, self.denominator = fraction.reduce(n, d)
+
+    @staticmethod
+    def gcd(a,b):
+        while b != 0:
+            a, b = b, a%b
+        return a
+    @classmethod
+    def reduce(cls, n1, n2):
+        g = cls.gcd(n1, n2)
+        return (n1 // g, n2 // g)
+    def __str__(self):
+        return str(self.numerator)+'/'+str(self.denominator)
