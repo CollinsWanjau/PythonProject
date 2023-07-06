df942fa0fd89b80dc73956f8a1543303230827f0
colloso999 <tripperskripper@gmail.com>
2023-07-06 09:51:16 +0300

    using static methods to retrieve

---
+++ classes/pet_class_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A problem lurks in the definition of about.The only way to access the class info
+__class_info is putting a class name in front of it.
+
+No matter what we do, the solution will not be what we want.
+
+We have no way of differentiating between the class Pet and its
+"""
+
+
+class Pet:
+    __class_info = "pet animals"
+    @staticmethod
+    def about():
+        print("This class is about " + Pet.__class_info + "!")
+class Dog(Pet):
+    __class_info = "man's best friends"
+class Cat(Pet):
+    __class_info = "all kinds of cats"
+Pet.about()
+Dog.about()
+Cat.about()
